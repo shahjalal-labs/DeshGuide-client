fb1a3bee04053a9ae326ae7fc07cf0ea0ab4046d
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-24 10:50:02 +0600

    improve: Updated console/log.json, router/ErrorPage.jsx

--- src/router/ErrorPage.jsx
+++ src/router/ErrorPage.jsx
@@ -11,12 +11,12 @@ const Errorpage = () => {
     <>
       <Navbar />
 
-      <main className="flex justify-center items-center min-h-[80vh] py-5 px-4 bg-[#0f172a] text-white">
+      <main className="flex justify-center items-center min-h-[90vh] py-5 px-4 bg-[#0f172a] text-white">
         <motion.div
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="max-w-3xl w-full text-center bg-gradient-to-br from-[#1e293b] to-[#0f172a] border border-purple-600 rounded-3xl shadow-[0_0_20px_#7c3aed66] p-8 md:p-14 space-y-6"
+          className="max-w-3xl w-full text-center bg-gradient-to-br from-[#1e293b] to-[#0f172a] border border-purple-600 rounded-3xl shadow-[0_0_20px_#7c3aed66]   p-8  space-y-6"
         >
           <h1 className="text-2xl md:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-indigo-500 to-sky-400 drop-shadow-lg">
             🚨 Oops! Something Went Wrong
