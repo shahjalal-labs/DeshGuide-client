07646b129f0e77bde04778252f8e6bbc611a35a8
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-25 18:29:35 +0600

    improve: Updated console/error.json, components/EditStoryModal.jsx

--- src/modules/Dashbaord/ManageStories/components/EditStoryModal.jsx
+++ src/modules/Dashbaord/ManageStories/components/EditStoryModal.jsx
@@ -31,7 +31,7 @@ const EditStoryModal = ({ story, onClose, refetch }) => {
   };
 
   return (
-    <dialog id="edit_modal" className="modal  " open>
+    <dialog id="edit_modal" className="modal  modal-start" open>
       <div className="modal-box bg-base-200">
         <h3 className="font-bold text-lg">Edit Story</h3>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-4">
@@ -69,66 +69,3 @@ const EditStoryModal = ({ story, onClose, refetch }) => {
 };
 
 export default EditStoryModal;
-/* // EditStoryModal.jsx
-import { useForm } from "react-hook-form";
-import useAxiosSecure from "../../../../hooks/useAxiosSecure";
-import { darkSwal } from "../../../../hooks/usePostData";
-
-const EditStoryModal = ({ story, onClose, refetch }) => {
-  const { register, handleSubmit, reset } = useForm({
-    defaultValues: {
-      title: story?.title,
-      description: story?.description,
-    },
-  });
-
-  const axiosSecure = useAxiosSecure();
-
-  const onSubmit = async (data) => {
-    try {
-      const res = await axiosSecure.put(`/stories/${story._id}`, data);
-      if (res.data?.modifiedCount > 0) {
-        darkSwal.fire("Updated!", "Your story has been updated.", "success");
-        refetch();
-        onClose();
-      }
-    } catch (err) {
-      darkSwal.fire("Error!", err.message, "error");
-    }
-  };
-
-  return (
-    <dialog id="edit_modal" className="modal modal-bottom sm:modal-middle" open>
-      <div className="modal-box bg-base-200">
-        <h3 className="font-bold text-lg">Edit Story</h3>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-4">
-          <input
-            {...register("title", { required: true })}
-            className="input input-bordered w-full"
-            placeholder="Title"
-          />
-          <textarea
-            {...register("description", { required: true })}
-            className="textarea textarea-bordered w-full"
-            placeholder="Description"
-            rows={4}
-          />
-          <div className="modal-action">
-            <button type="submit" className="btn btn-success btn-sm">
-              Update
-            </button>
-            <button
-              type="button"
-              onClick={onClose}
-              className="btn btn-ghost btn-sm"
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
-    </dialog>
-  );
-};
-
-export default EditStoryModal; */
