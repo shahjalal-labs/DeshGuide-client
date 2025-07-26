a052db6e7742bf49900021b840d1010877ba4639
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-26 16:47:31 +0600

    improve: Updated console/log.json, pages/Signin.jsx

--- src/modules/Auth/pages/Signin.jsx
+++ src/modules/Auth/pages/Signin.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 import SigninForm from "../components/SigninForm";
 import LottieAnimation from "../../../animation/LottieAnimation";
 import { authLottie } from "../../../animation";
@@ -12,25 +12,38 @@ import UseHelmet from "../../../hooks/useHelmet";
 
 const Signin = () => {
   return (
-    <div className="min-h-[80vh] mt-10 bg-gray-100 py-10 rounded-lg shadow">
+    <div className="min-h-[85vh] mt-10 px-4 py-12 bg-gradient-to-b from-[#0f0c29] via-[#302b63] to-[#24243e] rounded-2xl shadow-xl glow-border pulse-glow">
       <UseHelmet title="Sign in" />
-      <motion.div className="max-lg:max-w-[490px] mx-auto" {...topToBottom}>
-        <h2 className="text-center text-xl sm:text-3xl font-semibold font-kaushan text-purple-700">
-          Eduverse connects talent with opportunity.
+
+      {/* Heading */}
+      <motion.div
+        className="max-w-xl mx-auto text-center mb-10"
+        {...topToBottom}
+      >
+        <h2 className="text-3xl sm:text-4xl font-bold bg-gradient-to-r from-indigo-400 via-purple-500 to-blue-400 bg-clip-text text-transparent drop-shadow-md">
+          DeshGuide connects explorers with unforgettable journeys.
         </h2>
-        <p className="text-center text-gray-500 ext-sm mb-10 mt-2">
-          Unlock collaborative learning, connect with your study friends, and
-          ace your assignments together.
+        <p className="mt-3 text-sm text-gray-300">
+          Discover breathtaking destinations, trusted tour guides, and tailored
+          travel experiences — all in one platform.
         </p>
       </motion.div>
-      <div className="flex  flex-col-reverse lg:flex-row items-center justify-around *:flex-1 gap-7  mt-5">
+
+      {/* Main Content */}
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-around gap-10">
+        {/* Lottie */}
         <motion.div
-          className="lg:max-w-[480px]  order bg-gray-50 shadow-lg rounded-lg hover:shadow-sm hover:scale-98"
+          className="max-w-[480px] bg-gradient-to-br from-[#1e1e3f] via-[#2e2e5e] to-[#1e1e3f] rounded-xl shadow-lg transition-all duration-300 hover:scale-[1.015] hover:shadow-purple-500/20 glow-border"
           {...leftToRight}
         >
           <LottieAnimation anime={authLottie} />
         </motion.div>
-        <motion.div className="max-w-[480px]" {...rightToLeft}>
+
+        {/* Signin Form */}
+        <motion.div
+          className="max-w-[480px] w-full bg-base-10  hover:shadow-purple-500/10 transition-all duration-300"
+          {...rightToLeft}
+        >
           <SigninForm />
         </motion.div>
       </div>
