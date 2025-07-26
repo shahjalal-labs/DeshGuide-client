86f60c5f98b437c0586608fbf8c3cb9d71823569
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-26 19:59:33 +0600

    improve: Updated console/log.json, hooks/useUserRole.jsx

--- src/hooks/useUserRole.jsx
+++ src/hooks/useUserRole.jsx
@@ -14,7 +14,7 @@ const useUserRole = () => {
     queryKey: ["userRole", user?.email],
     enabled: !authLoading && !!user?.email,
     queryFn: async () => {
-      const res = await axiosSecure.get(`users/email/tourist4@example.com`);
+      const res = await axiosSecure.get(`users/email/${user?.email}`);
       return res.data.data; // should be the full user object
     },
   });
