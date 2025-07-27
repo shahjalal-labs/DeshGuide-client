75788ce568a67696774ab87d2be36ad24d0b18a1
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-27 11:38:20 +0600

    improve: Updated console/log.json, AssignedTour/AssignedTourCard.jsx

--- src/modules/Dashbaord/TourGuides/AssignedTour/AssignedTourCard.jsx
+++ src/modules/Dashbaord/TourGuides/AssignedTour/AssignedTourCard.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router";
 
 const AssignedTourCard = ({ booking, handleAction }) => {
   const {
@@ -98,97 +99,15 @@ const AssignedTourCard = ({ booking, handleAction }) => {
           // Reserve space to maintain height
           <div className="h-10"></div>
         )}
+        <Link
+          className="btn btn-sm btn-active btn-primary text-white hover:bg-white hover:text-black transition duration-200 rounded-full"
+          to={`/dashboard/bookings/${booking._id}`}
+        >
+          View Details
+        </Link>
       </div>
     </motion.div>
   );
 };
 
 export default AssignedTourCard;
-
-/* const AssignedTourCard = ({ booking, handleAction }) => {
-  const {
-    touristName,
-    touristEmail,
-    touristPhoto,
-    packageName,
-    price,
-    tourDate,
-    status,
-    paymentStatus,
-  } = booking;
-
-  const formattedDate = new Date(tourDate).toLocaleDateString();
-
-  return (
-    <div className="bg-purple-950 bg-opacity-60 rounded-xl p-5 shadow-lg border border-gray-700 hover:shadow-fuchsia-600 transition-all duration-300">
-      <div className="flex items-center gap-4 mb-4">
-        <img
-          src={touristPhoto}
-          alt={touristName}
-          className="w-14 h-14 rounded-full ring-2 ring-fuchsia-500 shadow-md"
-        />
-        <div>
-          <h2 className="text-lg font-semibold text-white">{touristName}</h2>
-          <p className="text-sm text-gray-400">{touristEmail}</p>
-        </div>
-      </div>
-
-      <div className="text-sm space-y-1 text-gray-300">
-        <p>
-          <span className="text-fuchsia-400 font-semibold">Package:</span>{" "}
-          {packageName}
-        </p>
-        <p>
-          <span className="text-fuchsia-400 font-semibold">Price:</span> ৳
-          {price}
-        </p>
-        <p>
-          <span className="text-fuchsia-400 font-semibold">Tour Date:</span>{" "}
-          {formattedDate}
-        </p>
-      </div>
-
-      <div className="mt-4 flex justify-between items-center">
-        <span
-          className={`badge ${
-            status === "accepted"
-              ? "badge-success"
-              : status === "rejected"
-                ? "badge-error"
-                : "badge-ghost"
-          }`}
-        >
-          {status}
-        </span>
-        <span
-          className={`badge ${
-            paymentStatus === "paid"
-              ? "badge-info"
-              : "badge-outline badge-warning"
-          }`}
-        >
-          {paymentStatus}
-        </span>
-      </div>
-
-      {booking.status === "in-review" && (
-        <div className="flex justify-end gap-4 mt-4 *:rounded-full">
-          <button
-            onClick={() => handleAction(booking._id, "accepted")}
-            className="btn btn-success btn-sm hover:brightness-110 transition"
-          >
-            Accept
-          </button>
-          <button
-            onClick={() => handleAction(booking._id, "rejected")}
-            className="btn btn-error btn-sm hover:brightness-110 transition"
-          >
-            Reject
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default AssignedTourCard; */
