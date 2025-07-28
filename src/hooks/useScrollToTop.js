8358b8ee137be5844052d4a010cd81250677981f
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-28 11:17:11 +0600

    improve: Updated console/error.json, console/log.json, hooks/useScrollToTop.js

--- src/hooks/useScrollToTop.js
+++ src/hooks/useScrollToTop.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+/* import { useEffect } from "react";
 import { useLocation } from "react-router";
 
 const useScrollToTop = () => {
@@ -11,6 +11,28 @@ const useScrollToTop = () => {
       left: 0,
       behavior: "smooth", // Optional smooth scrolling
     });
+  }, [pathname]);
+};
+
+export default useScrollToTop; */
+
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+
+const useScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top when path changes
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    // Fallback for immediate scroll if smooth doesn't work
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
   }, [pathname]);
 };
 
