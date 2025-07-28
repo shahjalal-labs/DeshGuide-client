9bbe567fa3a447eb2b063cf5bfc68f9ddbe261c3
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-28 16:25:20 +0600

    improve: Updated console/log.json, constants/footerLinks.js

--- src/modules/shared/Layout/Footer/constants/footerLinks.js
+++ src/modules/shared/Layout/Footer/constants/footerLinks.js
@@ -5,49 +5,39 @@ export const footerLinks = [
       {
         name: "GitHub",
         path: "https://github.com/shahjalal-labs/DeshGuide-client",
-        icon: "👨‍💻",
-        external: true
-      },
-      {
-        name: "Portfolio",
-        path: "http://shahjalal-labs.surge.sh",
-        icon: "🎨",
-        external: true
+        external: true,
       },
       {
         name: "LinkedIn",
         path: "https://www.linkedin.com/in/shahjalal-labs/",
-        icon: "💼",
-        external: true
+        external: true,
       },
       {
         name: "Twitter",
         path: "https://twitter.com/shahjalal_labs",
-        icon: "🐦",
-        external: true
+        external: true,
       },
       {
         name: "Facebook",
         path: "https://www.facebook.com/shahjalal.labs",
-        icon: "📘",
-        external: true
-      }
-    ]
+        external: true,
+      },
+    ],
   },
   {
     title: "Company",
     links: [
       { name: "About Us", path: "/about-us" },
       { name: "Contact Us", path: "/contact-us" },
-      { name: "Project Info", path: "/project-info" }
-    ]
+      { name: "Project Info", path: "/project-info" },
+    ],
   },
   {
     title: "Legal",
     links: [
       { name: "Terms of use", path: "/terms-of-use" },
       { name: "Privacy policy", path: "/privacy-policy" },
-      { name: "Cookie policy", path: "/cookie-policy" }
-    ]
-  }
+      { name: "Cookie policy", path: "/cookie-policy" },
+    ],
+  },
 ];
