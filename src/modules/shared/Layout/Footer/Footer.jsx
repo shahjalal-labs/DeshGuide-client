f73dcdb35cd2280a7fb2821aee8d05342621b487
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-29 08:04:06 +0600

    improve: Updated console/log.json, Footer/Footer.jsx | chore: Removed hooks/useFooterAnimation.js

--- src/modules/shared/Layout/Footer/Footer.jsx
+++ src/modules/shared/Layout/Footer/Footer.jsx
@@ -1,10 +1,7 @@
-import FooterColumns from './components/FooterColumns';
-import FooterCopyright from './components/FooterCopyright';
-import useFooterAnimation from './hooks/useFooterAnimation';
+import FooterColumns from "./components/FooterColumns";
+import FooterCopyright from "./components/FooterCopyright";
 
 const Footer = () => {
-  useFooterAnimation();
-  
   return (
     <div className="max-container">
       <footer className="bg-gradient-to-b from-[#0e0e0e] to-[#1a1a1a] text-gray-300 px-6 py-10">
