8c9736da38556ed4f6eb86f82ac494328b5b45e8
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-30 13:53:15 +0600

    improve: Updated console/log.json, Footer/Footer.jsx

--- src/modules/shared/Layout/Footer/Footer.jsx
+++ src/modules/shared/Layout/Footer/Footer.jsx
@@ -1,19 +1,3 @@
-/* import FooterColumns from "./components/FooterColumns";
-import FooterCopyright from "./components/FooterCopyright";
-
-const Footer = () => {
-  return (
-    <div className="max-container" data-aos="flip-up">
-      <footer className="bg-gradient-to-b from-[#0e0e0e] to-[#1a1a1a] text-gray-300 px-6 py-10">
-        <FooterColumns />
-      </footer>
-      <FooterCopyright />
-    </div>
-  );
-};
-
-export default Footer; */
-
 import FooterColumns from "./components/FooterColumns";
 import FooterCopyright from "./components/FooterCopyright";
 
